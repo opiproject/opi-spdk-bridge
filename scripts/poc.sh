ceeb76f16bcb64e535006dfa6b6c35c83e5199fb
Boris Glimcher <Boris.Glimcher@emc.com>
2022-07-25 20:07:13 +0300

    ci: storage POC
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- scripts/poc.sh
+++ scripts/poc.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-#
+# SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2022 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Copyright (c) 2022 Dell Inc, or its subsidiaries.
+
+set -euxo pipefail
+
+# DOCKER_COMPOSE setup
+DC=docker-compose
+
+if [ "$(command -v $DC)" == "" ]
+then
+    DC="docker compose"
+fi
 
 usage() {
-	echo "Usage: poc.sh [start|stop|tests|logs]"
+    echo "Usage: poc.sh [start|stop|tests|logs]"
 }
 
 tests_poc() {
-	docker-compose ps -a
+    $DC ps -a
     for i in $(seq 1 20)
     do
         echo "$i"
@@ -32,49 +30,49 @@ tests_poc() {
         fi
     done
     curl --fail --insecure --user spdkuser:spdkpass -X POST -H 'Content-Type: application/json' -d '{"id": 1, "method": "bdev_get_bdevs"}' http://127.0.0.1:9009
-	docker-compose ps -a
+    $DC ps -a
 }
 
 stop_poc() {
-	# Bring the containers down
-	docker-compose down
+    $DC down --volumes
+    docker network prune --force
 }
 
 start_poc() {
-    docker-compose down
-    docker network prune --force
-    docker-compose up -d
+    $DC up --detach
 }
 
 logs_poc() {
-    docker-compose ps -a
-    docker-compose logs || true
+    $DC ps -a
+    $DC logs || true
+    docker inspect "$($DC ps -q)" || true
     netstat -an || true
     ifconfig -a || true
 }
 
 stop_containers() {
-    bash -c "${DC} down --volumes"
+    $DC down --volumes
 }
 
 case ${1} in
-	start)
-		echo "Starting PoC"
-		start_poc
-		;;
-	stop)
-		echo "Stopping PoC"
-		stop_poc
-		;;
-	tests)
-		echo "Testing PoC"
-		tests_poc
-		;;
-	logs)
-		echo "Logs PoC"
-		logs_poc
-		;;
-	*)
-		usage
-		;;
+    start)
+        echo "Starting PoC"
+        stop_poc
+        start_poc
+        ;;
+    stop)
+        echo "Stopping PoC"
+        stop_poc
+        ;;
+    tests)
+        echo "Testing PoC"
+        tests_poc
+        ;;
+    logs)
+        echo "Logs PoC"
+        logs_poc
+        ;;
+    *)
+        usage
+        ;;
 esac
