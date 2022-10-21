191cd43554cc004a53c05c0f5058d6cd285568ee
Boris Glimcher <Boris.Glimcher@emc.com>
2022-10-21 19:59:33 +0300

    add tests
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ server/backend_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+)
+func TestBackEnd_NVMfRemoteControllerConnect(t *testing.T) {
+
+}
+
+func TestBackEnd_NVMfRemoteControllerDisconnect(t *testing.T) {
+
+}
+
+func TestBackEnd_NVMfRemoteControllerReset(t *testing.T) {
+
+}
+
+func TestBackEnd_NVMfRemoteControllerList(t *testing.T) {
+
+}
+
+func TestBackEnd_NVMfRemoteControllerGet(t *testing.T) {
+
+}
+
+func TestBackEnd_NVMfRemoteControllerStats(t *testing.T) {
+
+}
---
+++ server/spdk_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSpdk_Call(t *testing.T) {
+
+}
