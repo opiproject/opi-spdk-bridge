747c9efc0d7ae12f06f9f46986ec6e14feb17993
Boris Glimcher <Boris.Glimcher@emc.com>
2022-10-21 19:59:33 +0300

    add tests
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

---
+++ server/frontend_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFrontEnd_NVMeSubsystemCreate(t *testing.T) {
+
+}
+
+func TestFrontEnd_NVMeSubsystemDelete(t *testing.T) {
+
+}
+
+func TestFrontEnd_NVMeSubsystemUpdate(t *testing.T) {
+
+}
+
+func TestFrontEnd_NVMeSubsystemList(t *testing.T) {
+
+}
+
+func TestFrontEnd_NVMeSubsystemGet(t *testing.T) {
+
+}
+
+func TestFrontEnd_NVMeSubsystemStats(t *testing.T) {
+
+}
---
+++ server/middleend_test.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestMiddleEnd_Crypto(t *testing.T) {
+
+}
