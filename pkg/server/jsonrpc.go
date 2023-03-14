712bbf9dbee8ee85ee4979300dc39e86d572b938
Artsiom Koltun <artsiom.koltun@intel.com>
2023-03-14 14:54:46 +0100

    Use dedicated errors for SPDK failures.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/server/jsonrpc.go
+++ pkg/server/jsonrpc.go
@@ -15,6 +15,15 @@ import (
 	"sync/atomic"
 
 	"github.com/opiproject/opi-spdk-bridge/pkg/models"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	// ErrFailedSpdkCall indicates that the bridge failed to execute SPDK call
+	ErrFailedSpdkCall = status.Error(codes.Unknown, "Failed to execute SPDK call")
+	// ErrUnexpectedSpdkCallResult indicates that the bridge got an error from SPDK
+	ErrUnexpectedSpdkCallResult = status.Error(codes.FailedPrecondition, "Unexpected SPDK call result.")
 )
 
 // JSONRPC represents an interface to execute JSON RPC to SPDK
