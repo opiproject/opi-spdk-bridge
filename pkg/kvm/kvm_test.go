bea0c6ec13948bf67a3e03c792ef852c0f168429
Artsiom Koltun <artsiom.koltun@intel.com>
2023-03-14 15:18:00 +0100

    Add KVM add Virtio-Blk delete tests.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/kvm/kvm_test.go
+++ pkg/kvm/kvm_test.go
@@ -36,8 +36,9 @@ var (
 		VolumeId: &pc.ObjectKey{Value: "Malloc42"},
 		MaxIoQps: 1,
 	}}
-	genericQmpError = `{"error": {"class": "GenericError", "desc": "some error"}}` + "\n"
-	genericQmpOk    = `{"return": {}}` + "\n"
+	testDeleteVirtioBlkRequest = &pb.DeleteVirtioBlkRequest{Name: testVirtioBlkID}
+	genericQmpError            = `{"error": {"class": "GenericError", "desc": "some error"}}` + "\n"
+	genericQmpOk               = `{"return": {}}` + "\n"
 
 	qmpServerOperationTimeout = 500 * time.Millisecond
 	qmplibTimeout             = 250 * time.Millisecond
@@ -55,6 +56,15 @@ func (s stubJSONRRPC) Call(method string, args, result interface{}) error {
 				log.Panicf("Unexpected type for virtio-blk device creation result")
 			}
 			*resultCreateVirtioBLk = models.VhostCreateBlkControllerResult(true)
+		}
+		return s.err
+	} else if method == "vhost_delete_controller" {
+		if s.err == nil {
+			resultDeleteVirtioBLk, ok := result.(*models.VhostDeleteControllerResult)
+			if !ok {
+				log.Panicf("Unexpected type for virtio-blk device deletion result")
+			}
+			*resultDeleteVirtioBLk = models.VhostDeleteControllerResult(true)
 		}
 		return s.err
 	} else {
@@ -168,6 +178,25 @@ func (s *mockQmpServer) ExpectDeleteChardev(id string) *mockQmpServer {
 		response: genericQmpOk,
 		expectedArgs: []string{
 			`"execute":"chardev-remove"`,
+			`"id":"` + id + `"`,
+		},
+	})
+	return s
+}
+
+func (s *mockQmpServer) ExpectDeleteVirtioBlkWithEvent(id string) *mockQmpServer {
+	s.ExpectDeleteVirtioBlk(id)
+	s.expectedCalls[len(s.expectedCalls)-1].event =
+		`{"event":"DEVICE_DELETED","data":{"path":"/some/path","device":"` +
+			id + `"},"timestamp":{"seconds":1,"microseconds":2}}` + "\n"
+	return s
+}
+
+func (s *mockQmpServer) ExpectDeleteVirtioBlk(id string) *mockQmpServer {
+	s.expectedCalls = append(s.expectedCalls, mockCall{
+		response: genericQmpOk,
+		expectedArgs: []string{
+			`"execute":"device_del"`,
 			`"id":"` + id + `"`,
 		},
 	})
@@ -321,6 +350,101 @@ func TestCreateVirtioBlk(t *testing.T) {
 			wantOut, _ := proto.Marshal(test.out)
 			if !bytes.Equal(gotOut, wantOut) {
 				t.Errorf("Expected out %v, got %v", &test.out, out)
+			}
+			if !qmpServer.WereExpectedCallsPerformed() {
+				t.Errorf("Not all expected calls were performed")
+			}
+		})
+	}
+}
+
+func TestDeleteVirtioBlk(t *testing.T) {
+	tests := map[string]struct {
+		expectDeleteVirtioBlk          bool
+		expectDeleteVirtioBlkWithEvent bool
+		expectDeleteVirtioBlkError     bool
+
+		expectDeleteChardev      bool
+		expectDeleteChardevError bool
+
+		jsonRPC              server.JSONRPC
+		expectError          error
+		nonDefaultQmpAddress string
+	}{
+		"valid virtio-blk deletion": {
+			expectDeleteVirtioBlkWithEvent: true,
+			expectDeleteChardev:            true,
+			jsonRPC:                        alwaysSuccessfulJSONRPC,
+		},
+		"qemu device delete failed": {
+			expectDeleteVirtioBlkError: true,
+			expectDeleteChardev:        true,
+			jsonRPC:                    alwaysSuccessfulJSONRPC,
+			expectError:                errDevicePartiallyDeleted,
+		},
+		"qemu device delete failed by timeout": {
+			expectDeleteVirtioBlk: true,
+			expectDeleteChardev:   true,
+			jsonRPC:               alwaysSuccessfulJSONRPC,
+			expectError:           errDevicePartiallyDeleted,
+		},
+		"qemu chardev delete failed": {
+			expectDeleteVirtioBlkWithEvent: true,
+			expectDeleteChardevError:       true,
+			jsonRPC:                        alwaysSuccessfulJSONRPC,
+			expectError:                    errDevicePartiallyDeleted,
+		},
+		"spdk failed to delete virtio-blk": {
+			expectDeleteVirtioBlkWithEvent: true,
+			expectDeleteChardev:            true,
+			jsonRPC:                        alwaysFailingJSONRPC,
+			expectError:                    errDevicePartiallyDeleted,
+		},
+		"all qemu and spdk calls failed": {
+			expectDeleteVirtioBlkError: true,
+			expectDeleteChardevError:   true,
+			jsonRPC:                    alwaysFailingJSONRPC,
+			expectError:                errDeviceNotDeleted,
+		},
+		"failed to create monitor": {
+			nonDefaultQmpAddress: "/dev/null",
+			jsonRPC:              alwaysSuccessfulJSONRPC,
+			expectError:          errMonitorCreation,
+		},
+	}
+
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			opiSpdkServer := frontend.NewServer(test.jsonRPC)
+			opiSpdkServer.Virt.BlkCtrls[testVirtioBlkID] = testCreateVirtioBlkRequest.VirtioBlk
+			qmpServer := startMockQmpServer(t)
+			defer qmpServer.Stop()
+			qmpAddress := qmpServer.socketPath
+			if test.nonDefaultQmpAddress != "" {
+				qmpAddress = test.nonDefaultQmpAddress
+			}
+			kvmServer := NewServer(opiSpdkServer, qmpAddress, qmpServer.testDir)
+			kvmServer.timeout = qmplibTimeout
+
+			if test.expectDeleteVirtioBlkWithEvent {
+				qmpServer.ExpectDeleteVirtioBlkWithEvent(testVirtioBlkID)
+			}
+			if test.expectDeleteVirtioBlk {
+				qmpServer.ExpectDeleteVirtioBlk(testVirtioBlkID)
+			}
+			if test.expectDeleteVirtioBlkError {
+				qmpServer.ExpectDeleteVirtioBlk(testVirtioBlkID).WithErrorResponse()
+			}
+			if test.expectDeleteChardev {
+				qmpServer.ExpectDeleteChardev(testVirtioBlkID)
+			}
+			if test.expectDeleteChardevError {
+				qmpServer.ExpectDeleteChardev(testVirtioBlkID).WithErrorResponse()
+			}
+
+			_, err := kvmServer.DeleteVirtioBlk(context.Background(), testDeleteVirtioBlkRequest)
+			if !errors.Is(err, test.expectError) {
+				t.Errorf("Expected %v, got %v", test.expectError, err)
 			}
 			if !qmpServer.WereExpectedCallsPerformed() {
 				t.Errorf("Not all expected calls were performed")
