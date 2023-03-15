2512ce79fed796db6c9b9c165b7b0355b9bcc844
Boris Glimcher <Boris.Glimcher@emc.com>
2023-03-15 20:46:03 +0200

    tests: add test for json c-tor
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/server/jsonrpc_test.go
+++ pkg/server/jsonrpc_test.go
@@ -5,8 +5,48 @@
 package server
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestSpdk_NewUnixSocketJSONRPC(t *testing.T) {
+	tests := []struct {
+		name      string
+		address   string
+		transport string
+	}{
+		{
+			"testing unix",
+			"/var/tmp/spdk.sock",
+			"unix",
+		},
+		{
+			"testing tcp",
+			"10.10.10.1:1234",
+			"tcp",
+		},
+		{
+			"testing nonsense assuming unix",
+			"nonsense",
+			"unix",
+		},
+	}
+
+	// run tests
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			before := NewUnixSocketJSONRPC(tt.address)
+			after := &unixSocketJSONRPC{
+				transport: tt.transport,
+				socket:    &tt.address,
+				id:        0,
+			}
+			if !reflect.DeepEqual(before, after) {
+				t.Error("response: expected", after, "received", before)
+			}
+		})
+	}
+}
 
 func TestSpdk_Call(t *testing.T) {
 
