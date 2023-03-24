a1c8fb79b23330878431da1b2b4d2e09306a9f6b
Artsiom Koltun <artsiom.koltun@intel.com>
2023-03-24 12:55:34 +0100

    Move test environment creation into utils.
    Frontend, middleend and backend used the same procedures
    to prepare test environment. In this patch, this test
    environment creation is moved into utils.go file which can
    be used by other packages.
    
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/backend/backend_test.go
+++ pkg/backend/backend_test.go
@@ -7,85 +7,32 @@ package backend
 
 import (
 	"context"
-	"log"
-	"net"
-	"os"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/test/bufconn"
 
 	pb "github.com/opiproject/opi-api/storage/v1alpha1/gen/go"
 	"github.com/opiproject/opi-spdk-bridge/pkg/server"
 )
 
-// TODO: move test infrastructure code to a separate (test/server) package to avoid duplication
-
-type backendClient struct {
-	pb.NVMfRemoteControllerServiceClient
-	pb.NullDebugServiceClient
-	pb.AioControllerServiceClient
-}
-
 type testEnv struct {
+	*server.TestEnv
 	opiSpdkServer *Server
-	client        *backendClient
-	ln            net.Listener
-	testSocket    string
+	client        *server.TestOpiClient
 	ctx           context.Context
-	conn          *grpc.ClientConn
-	jsonRPC       server.JSONRPC
-}
-
-func (e *testEnv) Close() {
-	server.CloseListener(e.ln)
-	if err := os.RemoveAll(e.testSocket); err != nil {
-		log.Fatal(err)
-	}
-	server.CloseGrpcConnection(e.conn)
 }
 
 func createTestEnvironment(startSpdkServer bool, spdkResponses []string) *testEnv {
-	env := &testEnv{}
-	env.testSocket = server.GenerateSocketName("backend")
-	env.ln, env.jsonRPC = server.CreateTestSpdkServer(env.testSocket, startSpdkServer, spdkResponses)
-	env.opiSpdkServer = NewServer(env.jsonRPC)
-
-	ctx := context.Background()
-	conn, err := grpc.DialContext(ctx,
-		"",
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithContextDialer(dialer(env.opiSpdkServer)))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	env.ctx = ctx
-	env.conn = conn
-
-	env.client = &backendClient{
-		pb.NewNVMfRemoteControllerServiceClient(env.conn),
-		pb.NewNullDebugServiceClient(env.conn),
-		pb.NewAioControllerServiceClient(env.conn),
-	}
-
-	return env
-}
-
-func dialer(opiSpdkServer *Server) func(context.Context, string) (net.Conn, error) {
-	listener := bufconn.Listen(1024 * 1024)
-	server := grpc.NewServer()
-	pb.RegisterNVMfRemoteControllerServiceServer(server, opiSpdkServer)
-	pb.RegisterNullDebugServiceServer(server, opiSpdkServer)
-	pb.RegisterAioControllerServiceServer(server, opiSpdkServer)
-
-	go func() {
-		if err := server.Serve(listener); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	return func(context.Context, string) (net.Conn, error) {
-		return listener.Dial()
-	}
+	var opiSpdkServer *Server
+	env := server.CreateTestEnvironment(startSpdkServer, spdkResponses,
+		func(jsonRPC server.JSONRPC) server.TestOpiServer {
+			opiSpdkServer = NewServer(jsonRPC)
+			return server.TestOpiServer{
+				FrontendNvmeServiceServer:         &pb.UnimplementedFrontendNvmeServiceServer{},
+				FrontendVirtioBlkServiceServer:    &pb.UnimplementedFrontendVirtioBlkServiceServer{},
+				FrontendVirtioScsiServiceServer:   &pb.UnimplementedFrontendVirtioScsiServiceServer{},
+				MiddleendServiceServer:            &pb.UnimplementedMiddleendServiceServer{},
+				AioControllerServiceServer:        opiSpdkServer,
+				NullDebugServiceServer:            opiSpdkServer,
+				NVMfRemoteControllerServiceServer: opiSpdkServer,
+			}
+		})
+	return &testEnv{env, opiSpdkServer, env.Client, env.Ctx}
 }
