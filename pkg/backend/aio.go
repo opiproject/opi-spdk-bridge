3b7f67a3b85ab9bfdbd4d31d1994e8d2eccd1925
Boris Glimcher <Boris.Glimcher@emc.com>
2023-04-12 03:38:58 +0300

    List: limit in.PageSize to 250 by max
    According to Google AIP, max PageSize value
    shoould be limited to a reasonable max value
    
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/backend/aio.go
+++ pkg/backend/aio.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math"
 
 	pc "github.com/opiproject/opi-api/common/v1/gen/go"
 	pb "github.com/opiproject/opi-api/storage/v1alpha1/gen/go"
@@ -144,7 +145,7 @@ func (s *Server) ListAioControllers(_ context.Context, in *pb.ListAioControllers
 	}
 	size := 50
 	if in.PageSize > 0 {
-		size = int(in.PageSize)
+		size = int(math.Min(float64(in.PageSize), 250))
 	}
 	offset := 0
 	if in.PageToken != "" {
