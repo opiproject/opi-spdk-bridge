67315f484e5b2b374901831f540474a3d9dbe455
Artsiom Koltun <artsiom.koltun@intel.com>
2023-06-29 13:51:07 +0200

    Test backend Stats with non-AIP compliant names.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/backend/aio_test.go
+++ pkg/backend/aio_test.go
@@ -723,6 +723,14 @@ func TestBackEnd_AioControllerStats(t *testing.T) {
 			fmt.Sprintf("unable to find key %v", "unknown-id"),
 			false,
 		},
+		"malformed name": {
+			"-ABC-DEF",
+			nil,
+			[]string{""},
+			codes.Unknown,
+			fmt.Sprintf("segment '%s': not a valid DNS name", "-ABC-DEF"),
+			false,
+		},
 	}
 
 	// run tests
