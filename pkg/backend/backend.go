51595e9881015032fa141df60927cd7ea0c594eb
Boris Glimcher <Boris.Glimcher@emc.com>
2023-09-27 20:50:50 +0300

    feat(backend): replace Nvme path map with gokv.Store abstraction
    Signed-off-by: Boris Glimcher <Boris.Glimcher@emc.com>

--- pkg/backend/backend.go
+++ pkg/backend/backend.go
@@ -15,14 +15,6 @@ import (
 	pb "github.com/opiproject/opi-api/storage/v1alpha1/gen/go"
 )
 
-// TODO: can we combine all of volume types into a single list?
-//		 maybe create a volume abstraction like bdev in SPDK?
-
-// VolumeParameters contains all BackEnd volume related structures
-type VolumeParameters struct {
-	NvmePaths map[string]*pb.NvmePath
-}
-
 // Server contains backend related OPI services
 type Server struct {
 	pb.UnimplementedNvmeRemoteControllerServiceServer
@@ -31,7 +23,6 @@ type Server struct {
 
 	rpc        spdk.JSONRPC
 	store      gokv.Store
-	Volumes    VolumeParameters
 	Pagination map[string]int
 	psk        psk
 }
@@ -51,11 +42,8 @@ func NewServer(jsonRPC spdk.JSONRPC, store gokv.Store) *Server {
 		log.Panic("nil for Store is not allowed")
 	}
 	return &Server{
-		rpc:   jsonRPC,
-		store: store,
-		Volumes: VolumeParameters{
-			NvmePaths: make(map[string]*pb.NvmePath),
-		},
+		rpc:        jsonRPC,
+		store:      store,
 		Pagination: make(map[string]int),
 		psk: psk{
 			createTempFile: os.CreateTemp,
--- pkg/backend/nvme_path_test.go
+++ pkg/backend/nvme_path_test.go
@@ -232,7 +232,7 @@ func TestBackEnd_CreateNvmePath(t *testing.T) {
 			testEnv.opiSpdkServer.store.Set(testNvmeCtrlName, utils.ProtoClone(tt.controller))
 
 			if tt.exist {
-				testEnv.opiSpdkServer.Volumes.NvmePaths[testNvmePathName] = utils.ProtoClone(&testNvmePathWithName)
+				testEnv.opiSpdkServer.store.Set(testNvmePathName, &testNvmePathWithName)
 			}
 			if tt.out != nil {
 				tt.out = utils.ProtoClone(tt.out)
@@ -451,7 +451,7 @@ func TestBackEnd_DeleteNvmePath(t *testing.T) {
 			testEnv := createTestEnvironment(tt.spdk)
 			defer testEnv.Close()
 
-			testEnv.opiSpdkServer.Volumes.NvmePaths[testNvmePathName] = utils.ProtoClone(&testNvmePathWithName)
+			testEnv.opiSpdkServer.store.Set(testNvmePathName, &testNvmePathWithName)
 			testEnv.opiSpdkServer.store.Set(testNvmeCtrlName, &testNvmeCtrlWithName)
 
 			request := &pb.DeleteNvmePathRequest{Name: tt.in, AllowMissing: tt.missing}
@@ -637,7 +637,7 @@ func TestBackEnd_UpdateNvmePath(t *testing.T) {
 			testEnv := createTestEnvironment(tt.spdk)
 			defer testEnv.Close()
 
-			testEnv.opiSpdkServer.Volumes.NvmePaths[testNvmePathName] = utils.ProtoClone(&testNvmePathWithName)
+			testEnv.opiSpdkServer.store.Set(testNvmePathName, &testNvmePathWithName)
 
 			request := &pb.UpdateNvmePathRequest{NvmePath: tt.in, UpdateMask: tt.mask, AllowMissing: tt.missing}
 			response, err := testEnv.client.UpdateNvmePath(testEnv.ctx, request)
@@ -950,7 +950,7 @@ func TestBackEnd_GetNvmePath(t *testing.T) {
 			testEnv := createTestEnvironment(tt.spdk)
 			defer testEnv.Close()
 
-			testEnv.opiSpdkServer.Volumes.NvmePaths[testNvmePathName] = utils.ProtoClone(&testNvmePathWithName)
+			testEnv.opiSpdkServer.store.Set(testNvmePathName, &testNvmePathWithName)
 
 			request := &pb.GetNvmePathRequest{Name: tt.in}
 			response, err := testEnv.client.GetNvmePath(testEnv.ctx, request)
@@ -1053,7 +1053,7 @@ func TestBackEnd_StatsNvmePath(t *testing.T) {
 			testEnv := createTestEnvironment(tt.spdk)
 			defer testEnv.Close()
 
-			testEnv.opiSpdkServer.Volumes.NvmePaths[testNvmePathName] = utils.ProtoClone(&testNvmePathWithName)
+			testEnv.opiSpdkServer.store.Set(testNvmePathName, &testNvmePathWithName)
 
 			request := &pb.StatsNvmePathRequest{Name: tt.in}
 			response, err := testEnv.client.StatsNvmePath(testEnv.ctx, request)
