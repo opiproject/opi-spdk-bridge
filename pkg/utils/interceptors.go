770ed22e865f578f135e8e1742001d973be9237b
Artsiom Koltun <artsiom.koltun@intel.com>
2023-09-28 11:56:10 +0200

    feat: add grpc interceptor to log calls
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

---
+++ pkg/utils/interceptors.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+)
+
+// InterceptorLogger creates logger for interceptors based on default Go logger
+func InterceptorLogger(l *log.Logger) logging.Logger {
+	return logging.LoggerFunc(func(_ context.Context, lvl logging.Level, msg string, fields ...any) {
+		switch lvl {
+		case logging.LevelDebug:
+			msg = fmt.Sprintf("DEBUG :%v", msg)
+		case logging.LevelInfo:
+			msg = fmt.Sprintf("INFO :%v", msg)
+		case logging.LevelWarn:
+			msg = fmt.Sprintf("WARN :%v", msg)
+		case logging.LevelError:
+			msg = fmt.Sprintf("ERROR :%v", msg)
+		default:
+			panic(fmt.Sprintf("unknown level %v", lvl))
+		}
+		l.Println(append([]any{"msg", msg}, fields...))
+	})
+}
