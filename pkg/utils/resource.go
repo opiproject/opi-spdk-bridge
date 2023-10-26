2f05848ad1c41402de3812dbec93b4eff72f9563
Artsiom Koltun <artsiom.koltun@intel.com>
2023-10-26 17:16:22 +0200

    refactor: use aip-go Join for resource names
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/utils/resource.go
+++ pkg/utils/resource.go
@@ -6,32 +6,45 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
+
+	"go.einride.tech/aip/resourcename"
 )
 
 // ResourceIDToVolumeName creates name of volume resource based on ID
 func ResourceIDToVolumeName(resourceID string) string {
-	return fmt.Sprintf("//storage.opiproject.org/volumes/%s", resourceID)
+	return resourcename.Join(
+		"//storage.opiproject.org/",
+		"volumes", resourceID,
+	)
 }
 
 // ResourceIDToSubsystemName transforms subsystem resource ID to subsystem name
 func ResourceIDToSubsystemName(resourceID string) string {
-	return fmt.Sprintf("//storage.opiproject.org/subsystems/%s", resourceID)
+	return resourcename.Join(
+		"//storage.opiproject.org/",
+		"subsystems", resourceID,
+	)
 }
 
 // ResourceIDToNamespaceName transforms subsystem resource ID and namespace
 // resource ID to namespace name
 func ResourceIDToNamespaceName(subsysResourceID, ctrlrResourceID string) string {
-	return fmt.Sprintf("//storage.opiproject.org/subsystems/%s/namespaces/%s",
-		subsysResourceID, ctrlrResourceID)
+	return resourcename.Join(
+		"//storage.opiproject.org/",
+		"subsystems", subsysResourceID,
+		"namespaces", ctrlrResourceID,
+	)
 }
 
 // ResourceIDToControllerName transforms subsystem resource ID and controller
 // resource ID to controller name
 func ResourceIDToControllerName(subsysResourceID, ctrlrResourceID string) string {
-	return fmt.Sprintf("//storage.opiproject.org/subsystems/%s/controllers/%s",
-		subsysResourceID, ctrlrResourceID)
+	return resourcename.Join(
+		"//storage.opiproject.org/",
+		"subsystems", subsysResourceID,
+		"controllers", ctrlrResourceID,
+	)
 }
 
 // GetSubsystemIDFromNvmeName get parent ID (subsystem ID) from nvme related names
